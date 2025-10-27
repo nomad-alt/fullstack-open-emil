04f356e38fe55977098a5babde6499c2cfba1a0a
nomad-alt <nomad-alt@proton.me>
2025-10-27 09:13:54 +0100

    The whole app to the internet

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -26,6 +27,8 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
   if (request.method !== 'POST') {
@@ -118,7 +121,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
