ba4d488c6c9c42b6c1cbeed1c00645fa5c912968
nomad-alt <nomad-alt@proton.me>
2025-11-05 13:10:24 +0100

    added part4 folder

---
+++ part4/blog-list/controllers/blogs.js
@@ -0,0 +1,73 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs)
+    })
+    .catch((error) => next(error))
+})
+
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (!blog) {
+        return response.status(404).end()
+      }
+      response.json(blog)
+    })
+    .catch((error) => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes ?? 0,
+  })
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.status(201).json(savedBlog)
+    })
+    .catch((error) => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      if (!result) {
+        return response.status(404).end()
+      }
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    }
+  )
+    .then((updatedBlog) => {
+      if (!updatedBlog) {
+        return response.status(404).end()
+      }
+      response.json(updatedBlog)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = blogsRouter
